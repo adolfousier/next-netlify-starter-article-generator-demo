8c9302757e7bc73e0c41a61c595f750460675c73
Adolfo Usier <35583002+adolfousier@users.noreply.github.com>
2023-10-12 18:07:37 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Mint Ventures AI Demo!</title>
+        <link rel="icon" href="/MV_favicon.svg" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Mint Ventures AI Article Generator Demo!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Type a topic and I will write the article! Get started by clicking on the bot icon.
         </p>
       </main>
 
